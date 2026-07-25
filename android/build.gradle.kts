eca76e7d22aad541faf6f41bf7155f43683e3efe
SP Admin <admin@spnewsmaregaon.com>
2026-07-25 08:12:17 +0000

    Fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,6 +11,16 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    afterEvaluate {
+        if (hasProperty("android")) {
+            configure<com.android.build.gradle.LibraryExtension> {
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
